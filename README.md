876d369830fbb3c7b5a0d1b2c83dd104807e7fe8
ruoli <18663258575@@163.com>
2022-11-30 10:51:08 +0800

    demo

---
+++ README.md
@@ -0,0 +1,22 @@
+### 运行demo
+
+1. python版本: 3.6.10
+
+2. 启动流程:
+
+   ```
+   cd zone-python-demo
+   pip install -r requirements.txt
+   python3 demo.py
+   ```
+
+3. 默认5000端口
+
+### 接入流程
+
+1. 获取key   请前往https://0.zone/applyParticulars?type=site查看
+2. 提交认证企业  前往https://0.zone/dataService查看
+
+
+
+ 
---
+++ demo.py
@@ -0,0 +1,66 @@
+import json
+import requests
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+
+class Main:
+    def __init__(self) -> None:
+        self.base_url = "https://0.zone/"
+        self.data_url = self.base_url + "api/plug/data/"  # 数据url
+        self.aggs_url = self.base_url + "api/plug/aggs/"  # 聚合数据url
+
+    def getData(self, data: dict, request_type: int=0) -> dict:
+        if request_type:
+            request_url = self.data_url
+        else:
+            request_url = self.aggs_url
+
+        try:
+            response = requests.post(url=request_url, json=data, timeout=10)
+        except Exception as e:
+            raise e
+        
+        return json.loads(response.text)
+
+
+@app.route("/data", methods=["POST"])
+def data():
+    data = request.json
+    request_data = dict(
+        title=data.get("title", ""), # 搜索条件
+        title_type=data.get("title_type", ""), # 搜索类型 目前只支持site(信息系统), domain(域名)
+        company=data.get("company", ""), # 认证公司
+        page=data.get("page", 1), # 页数
+        pagesize=data.get("pagesize", 10), # 页大小
+        key=data.get("key", "") # 0.zone key
+    )
+    try:
+        data = Main().getData(request_data, request_type=1)      
+    except:
+        return jsonify(dict(code=1, message="获取数据失败"))
+
+    return jsonify(data)
+
+@app.route("/aggs", methods=["POST"])
+def aggs():
+    data = request.json
+    request_data = dict(
+        title=data.get("title", ""),
+        title_type=data.get("title_type", ""),
+        company=data.get("company", ""),
+        key=data.get("key", "")
+    )
+    try:
+        data = Main().getData(request_data)
+    except:
+        return jsonify(dict(code=1, message="获取聚合数据失败"))
+    
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
